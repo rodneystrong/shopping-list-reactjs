936328edd7f1ae066381bb87ea4788d22a1123b5
aleeex <akang2@gmail.com>
2014-12-08 16:05:26 -0800

    added code for deleting items

--- app/js/react-list.js
+++ app/js/react-list.js
@@ -6,7 +6,7 @@ var ShopList = React.createClass({
 				{text: "default whatever"},
 				{text: "user-input"}
 					],
-		}
+		} /*right here we're returning an object with 'list' as the key. important to remember here. */
 	},
 	addItem: function(e) {
 		//checking for what each keypress code is
@@ -14,7 +14,7 @@ var ShopList = React.createClass({
 		if (e.keyCode==13) {
 			var lists = this.state.lists;
 			lists.unshift({text: e.target.value});
-			this.setState({lists: lists});
+			this.setState({lists: lists}); /*because we returned 'list' as an object above in the 'getInitialState' function, we're setting its state in object notation here*/
 		}
 	},
 	render: function() {
@@ -32,11 +32,15 @@ var ShopList = React.createClass({
 });
 
 var ListContainer = React.createClass({
+	removeItem: function(index) {
+		console.log(index);
+	},
 	renderLists: function() {
+		var self = this; //doing this because we can't use 'this' inside the map function because of function scope. map is a function inside the 'renderLists' function
 		//this is a looping function (map) that will go through the 'lists' array for you
-		return this.props.lists.map(function(list) {
+		return this.props.lists.map(function(list, index) {
 			return (
-				<ListItem text={list.text} />
+				<ListItem text={list.text} index={index} removeItem={self.removeItem} />
 			)
 		}) 
 	},
@@ -54,17 +58,46 @@ var ListContainer = React.createClass({
 });
 
 var ListItem = React.createClass({
+	getInitialState: function() {
+		return ({checked: false})
+	},
+	toggleCheck: function() {
+		this.setState({checked: !this.state.checked}) /* huh?! */
+	},
+	removeItem: function() {
+		console.log("removeItem");
+		console.log(this.props.index);
+		/*in react you always work UP. You pass data from the bottom up; one-direction data flow is the react paradigm*/
+		this.props.removeItem(this.props.index);
+	},
 	render: function() {
+
 		return (
 			<div className="checkbox m-bot">
-			  <span className="close-button"><i className="fa fa-close"></i></span>
-			  <label className="item-width">
-			    <input className="check" type="checkbox" value="" />
+			  <span onClick={this.removeItem} className="close-button"><i className="fa fa-close"></i></span>
+			  <label className={this.state.checked ? "item-width strikethrough" : "item-width"}>
+			    <input onClick={this.toggleCheck} className="check" type="checkbox" value="" />
 			    {this.props.text}
 			  </label>
 			</div>
 		)
 	}
+	//this is for if we don't want to use a ternary operator
+	// render: function() {
+	// 	var customClass = "item-width";
+	// 	if(this.state.checked) {
+	// 		customClass= "item-width strikethrough";
+	// 	}
+	// 	return (
+	// 		<div className="checkbox m-bot">
+	// 		  <span className="close-button"><i className="fa fa-close"></i></span>
+	// 		  <label className={customClass}>
+	// 		    <input onClick={this.toggleCheck} className="check" type="checkbox" value="" />
+	// 		    {this.props.text}
+	// 		  </label>
+	// 		</div>
+	// 	)
+	// }
 })
 
 React.render(
